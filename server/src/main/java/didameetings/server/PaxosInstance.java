af2efecc22b0f8f8cc03724c35e765695bc033d9
kuukitenshi <lauracunha803@outlook.pt>
2025-09-20 20:36:24 +0100

    feat: initial multipaxos - leader issue

--- server/src/main/java/didameetings/server/PaxosInstance.java
+++ server/src/main/java/didameetings/server/PaxosInstance.java
@@ -1,10 +1,12 @@
 package didameetings.server;
 
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PaxosInstance {
 
     public int instanceId = -1;
+    
     public int commandId = -1;
     public int readBallot = -1;
     public int writeBallot = -1;
@@ -12,6 +14,9 @@ public class PaxosInstance {
     public int numAccepts = 0;
     public boolean decided = false;
     public boolean valueLocked = false;
+    
+    public List<Integer> commandIds = new ArrayList<>();
+    public boolean needsPhaseTwo = true;
 
     public PaxosInstance(int id) {
         this(id, -1);
@@ -20,5 +25,32 @@ public class PaxosInstance {
     public PaxosInstance(int id, int ballot) {
         this.instanceId = id;
         this.readBallot = ballot;
+    }
+
+    public void addCommand(int commandId) {
+        if (!commandIds.contains(commandId)) {
+            commandIds.add(commandId);
+        }
+    }
+    
+    public int getCommandId() {
+        return commandIds.isEmpty() ? commandId : commandIds.get(0);
+    }
+    
+    public void setCommandId(int cmd) {
+        this.commandId = cmd;
+        if (commandIds.isEmpty()) {
+            commandIds.add(cmd);
+        } else {
+            commandIds.set(0, cmd);
+        }
+    }
+    
+    public boolean shouldRunPhaseTwo(int currentBallot, int currentLeader, int previousLeader) {
+        return needsPhaseTwo || (previousLeader != -1 && previousLeader != currentLeader);
+    }
+    
+    public void markPhaseTwoExecuted() {
+        this.needsPhaseTwo = false;
     }
 }
