4563174a987054e2bb67d25a713cf0f165dc6caf
Laura Cunha <laura.r.velasco.cunha@tecnico.ulisboa.pt>
2025-10-17 11:37:49 +0100

    fix: script zip with report

--- make_zip.sh
+++ make_zip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mvn clean eclipse:clean
-zip -r dida-meetings-group11.zip app/ configs/ console/ contract/ core/ server/ util/ README.md pom.xml setup_env.sh
+zip -r dida-meetings-group11.zip app/ configs/ console/ contract/ core/ server/ util/ README.md pom.xml setup_env.sh report_g11.pdf
